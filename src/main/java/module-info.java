fe422467110ef26136a63527caed4e1e9eab5b0f
Jacob <silveriolance06@gmail.com>
2025-04-14 18:14:07 +0800

    Deleted module file

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.hirayadeskbeta {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.hirayadeskbeta to javafx.fxml;
-    exports com.mycompany.hirayadeskbeta;
-}
